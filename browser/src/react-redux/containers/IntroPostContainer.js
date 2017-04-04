eb8709cff04d8a3edec483f5ce900c332c575048
Don Hansen <donhansen347@gmail.com>
2017-04-04 07:21:56 -0500

    added hr tag in map

--- browser/src/react-redux/containers/IntroPostContainer.js
+++ browser/src/react-redux/containers/IntroPostContainer.js
@@ -3,12 +3,19 @@ import React from 'react';
 import IntroPost from '../components/IntroPost';
 
 const IntroPostContainer = (props) => {
+  const maxIndex = props.posts.length-1;
+
   return (
     <div>
       {
-        props.posts.map((post, index) =>
-          <IntroPost key={index} post={post} />
-        )
+        props.posts.map((post, index) => {
+          return (
+          <div key={post.id}>
+            <IntroPost index={index} post={post} />
+            {index < maxIndex && <hr />}
+          </div>
+          )
+        })
       }
     </div>
   )
