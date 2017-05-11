3a2161d19cfbbb82d0531544985d7c9c595a928a
Don Hansen <donhansen347@gmail.com>
2017-05-11 17:54:54 -0500

    name is now displayed in commentcard component

--- browser/src/react-redux/components/CommentCard.js
+++ browser/src/react-redux/components/CommentCard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const CommentCard = (props) => {
+  const firstName = props.comment.author.first_name;
+  const lastName = props.comment.author.last_name;
+
   return (
     <li>
-      <p></p>
+      <p>{`${firstName} ${lastName}`}</p>
       <p>{props.comment.content}</p>
     </li>
   );
