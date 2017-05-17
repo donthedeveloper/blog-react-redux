42878bb7f5a5ee5ee0766743057214b39c2a5895
Don Hansen <donhansen347@gmail.com>
2017-05-17 01:30:21 -0500

    test

--- server/models/db.js
+++ server/models/db.js
@@ -1,5 +1,9 @@
+const {dbConfig} = require('../config');
+
 const Sequelize = require('sequelize');
-// const db = new Sequelize('postgres://localhost:5432/blog_react_redux');
-const db = new Sequelize('postgres://gampoglpucnkym:dda9a24aec17c3922b9b5fe81ebb7a7a6c9246961173d2f46dd370613adc55ea@ec2-107-20-141-145.compute-1.amazonaws.com:5432/d5i2uhdgu3hvdi');
+const db = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    host: dbConfig.host, 
+    dialect: 'postgres'
+});
 
 module.exports = { Sequelize, db };
