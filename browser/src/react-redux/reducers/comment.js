372795c3c293da51b951c83aeb928aed23a96598
Don Hansen <donhansen347@gmail.com>
2017-05-17 18:02:06 -0500

    now handles empty comments on front end and backend

--- browser/src/react-redux/reducers/comment.js
+++ browser/src/react-redux/reducers/comment.js
@@ -2,18 +2,23 @@ import axios from 'axios';
 
 
 const initialState = ({
-  comments: []
+  comments: [], 
+  errorMessage: ''
 });
 
 
 export default (state=initialState, action) => {
   const newState = Object.assign({}, state);
 
-  // console.log('action:', action.comments);
-
   switch (action.type) {
     case RETRIEVE_COMMENTS:
       newState.comments = action.comments;
+      break;
+    case UPDATE_ERROR_MESSAGE:
+      newState.errorMessage = action.errorMessage;
+      break;
+    case RESET_ERROR_MESSAGE:
+      newState.errorMessage = '';
       break;
     default:
       return state;
@@ -24,11 +29,22 @@ export default (state=initialState, action) => {
 
 
 const RETRIEVE_COMMENTS = 'RETRIEVE_COMMENTS';
+const UPDATE_ERROR_MESSAGE = 'UPDATE_ERROR_MESSAGE';
+const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE';
 
 
 const retrieve = (comments) => ({
   type: RETRIEVE_COMMENTS,
   comments
+});
+
+const updateError = (errorMessage) => ({
+  type: UPDATE_ERROR_MESSAGE, 
+  errorMessage
+});
+
+const resetError = () => ({
+  type: RESET_ERROR_MESSAGE
 });
 
 
@@ -37,6 +53,7 @@ export const retrieveComments = (postId) =>
     axios.get(`/api/comments?postId=${postId}`)
       .then((comments) => {
         dispatch(retrieve(comments.data));
+        dispatch(resetError());
       })
       .catch((err) => {
         console.error(err.message);
@@ -49,22 +66,28 @@ export const addComment = (comment) =>
         dispatch(retrieveComments(comment.data.postId));
       })
       .catch((err) => {
+        const statusCode = err.response.status;
+        const errorMessage = err.response.request.responseText;
+
+        if (statusCode === 400) {
+          dispatch(updateError(errorMessage));
+        }
         console.error(err.message);
       });
 
-export const updateComment = (comment) =>
-  dispatch =>
-    axios.put(`/api/comments/${comment.id}`, comment)
-      .then((statusObj) => {
-        if (statusObj.status === 200) {
-          dispatch(retrieveCommenents());
-        }
-      })
-      .catch((err) => {
-        console.error(error.message)
-      });
+// export const updateComment = (comment) =>
+//   dispatch =>
+//     axios.put(`/api/comments/${comment.id}`, comment)
+//       .then((statusObj) => {
+//         if (statusObj.status === 200) {
+//           dispatch(retrieveCommenents());
+//         }
+//       })
+//       .catch((err) => {
+//         console.error(error.message)
+//       });
 
-export const deleteComment = (commentId) => {
-  dispatch =>
-    axios.delete('/api/comments/')
-}
+// export const deleteComment = (commentId) => {
+//   dispatch =>
+//     axios.delete('/api/comments/')
+// }
