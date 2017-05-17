518740390124582b01d0b6d8ee0b660dcc79ab14
Don Hansen <donhansen347@gmail.com>
2017-05-17 00:40:34 -0500

    added database support on heroku

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/blog_react_redux');
+// const db = new Sequelize('postgres://localhost:5432/blog_react_redux');
+const db = new Sequelize('postgres://gampoglpucnkym:dda9a24aec17c3922b9b5fe81ebb7a7a6c9246961173d2f46dd370613adc55ea@ec2-107-20-141-145.compute-1.amazonaws.com:5432/d5i2uhdgu3hvdi');
 
 module.exports = { Sequelize, db };
