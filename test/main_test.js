59757ddbb1495ad57e5062a5b71fc29567c1f4a6
Don Hansen <donhansen347@gmail.com>
2017-06-21 07:45:51 -0500

    mocha and chai now working as expected

---
+++ test/main_test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+// Test suite
+describe('Mocha', function() {
+    // Test spec (unit test)
+    it('should run our tests using npm', function() {
+        expect(true).to.be.ok;
+    });
+});
