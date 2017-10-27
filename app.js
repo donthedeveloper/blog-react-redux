a24a83bc8e4b41395f6364f5ad7bbd7fcc216623
Don Hansen <donhansen347@gmail.com>
2017-10-26 21:35:16 -0500

    fixed typeerror appearing when importing files in tests

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const session = require('client-sessions');
 const nunjucks = require('nunjucks');
 const chalk = require('chalk');
 
+require('dotenv').config()
+
 const router = require('./server/routes');
 
 // middleware
